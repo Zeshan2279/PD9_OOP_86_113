c5417200a9d3a8001a351d1df37fb1de7a5a4f2a
TalhaRamzan <talharamzan640@gmail.com>
2021-04-24 18:21:10 +0500

    Task3

---
+++ src/pd9/Task3.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pd9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Task3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static String ridMultipleBlank(String s) {
+        String Blank = " ";
+        char B = ' ';
+        int count=1;
+        for (int i=0;i<s.length();i++)
+        {   
+            if(count==1||B!=' ')
+            {
+            B=s.charAt(i);
+            Blank=Blank+B;
+            count=0;
+            }
+            if(B!=' ')
+            {
+                count=1;
+            }
+           else if(B==' ')
+            {
+                count=0;
+            }
+            
+        
+              
+        
+        }
+            
+           
+        
+        
+        return Blank;
+    }
+    public static void main(String[] args) {
+        Scanner cin= new Scanner(System.in);
+        System.out.println("InputString: (To Remove Spaces)\n ");
+        String S=cin.nextLine();
+        System.out.println(ridMultipleBlank(S));
+        
+        
+    }
+
+    private static String toString(char charAt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
