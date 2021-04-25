2a42360e2302a684ccc7c0f001f0338117fc9b07
Zeshan Ayub <unofficialmz@gmail.com>
2021-04-25 14:04:18 +0500

    MyString 3rd Function
    3rd function added to MyString class of Task 4

--- src/pd9/MyString.java
+++ src/pd9/MyString.java
@@ -12,23 +12,27 @@ import java.util.Scanner;
  * @author YDRGZM
  */
 public class MyString {
-    
+
     public static void main(String[] args) {
-        Scanner cin=new Scanner(System.in);
+        Scanner cin = new Scanner(System.in);
         System.out.println("Enter String (Check if it is alphabetic): ");
-        String s=cin.nextLine();
-        if(isAlpha(s)){
+        String s = cin.nextLine();
+        if (isAlpha(s)) {
             System.out.println("Given String is only alphabetic");
-        }else{
+        } else {
             System.out.println("Given String is not only alphabetic");
         }
-        
+
         System.out.println("\nEnter String (to capitalize each word of sentence): ");
-        s=cin.nextLine();
-        System.out.println("Output: "+"\n"+capitalizeEachWord(s));
+        s = cin.nextLine();
+        System.out.println("Output: " + "\n" + capitalizeEachWord(s));
         
+        System.out.println("\nEnter String (Toggle Case): ");
+        s = cin.nextLine();
+        System.out.println("Output: " + "\n" + toggleCase(s));
         
     }
+
     static boolean isAlpha(String s) {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
@@ -38,6 +42,7 @@ public class MyString {
         }
         return true;
     }
+
     static String capitalizeEachWord(String s) {
         String str = "";
         boolean flag = true;
@@ -57,5 +62,19 @@ public class MyString {
         }
         str += s.charAt(s.length() - 1);
         return str;
+    }
+    static String toggleCase(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLowerCase(c)) {
+                result += Character.toUpperCase(c);
+            } else if (Character.isUpperCase(c)) {
+                result += Character.toLowerCase(c);
+            } else {
+                result += c;
+            }
+        }
+        return result;
     }
 }
