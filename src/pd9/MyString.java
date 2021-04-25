59c7d22ffcf97ee50ed555ac61c73811a2773306
Zeshan Ayub <unofficialmz@gmail.com>
2021-04-25 14:00:29 +0500

    MyString 2nd Function
    Second function added to Task 4

--- src/pd9/MyString.java
+++ src/pd9/MyString.java
@@ -15,13 +15,19 @@ public class MyString {
     
     public static void main(String[] args) {
         Scanner cin=new Scanner(System.in);
-        System.out.println("Enter String: ");
+        System.out.println("Enter String (Check if it is alphabetic): ");
         String s=cin.nextLine();
         if(isAlpha(s)){
             System.out.println("Given String is only alphabetic");
         }else{
             System.out.println("Given String is not only alphabetic");
         }
+        
+        System.out.println("\nEnter String (to capitalize each word of sentence): ");
+        s=cin.nextLine();
+        System.out.println("Output: "+"\n"+capitalizeEachWord(s));
+        
+        
     }
     static boolean isAlpha(String s) {
         for (int i = 0; i < s.length(); i++) {
@@ -31,5 +37,25 @@ public class MyString {
             }
         }
         return true;
+    }
+    static String capitalizeEachWord(String s) {
+        String str = "";
+        boolean flag = true;
+        char c = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+
+            if (flag) {
+                str += Character.toUpperCase(c);
+                flag = false;
+            } else {
+                str += c;
+            }
+            c = s.charAt(i);
+            if (s.charAt(i - 1) == ' ') {
+                flag = true;
+            }
+        }
+        str += s.charAt(s.length() - 1);
+        return str;
     }
 }
